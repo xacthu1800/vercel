71f07af9a2a6d942c40625e642f119995a9bf670
Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-05-23 00:57:22 +0700

    fasdf

--- route/index.js
+++ route/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const express = require('mongoose')
+const mongoose = require('mongoose')
 const {dataUser, dataProduct, delivery, record} = require('../src/config');
 
 const router = express.Router();
