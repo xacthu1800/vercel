e028ad96372f708f60fb680e9ec60bbdd666649c
Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-05-23 00:44:59 +0700

    asdfsd

---
+++ route/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const {dataUser, dataProduct, delivery, record} = require('../src/config');
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+    const product = await dataProduct.find().sort({ _id: -1 }).limit(12);
+    return     res.render("index", { pros: product, userN: req.session.username, login: "login", logout: "logout" });
+});
+
+module.exports = router;
