ac21e12000358972e26e0de5a982a9bca06163e8
Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-05-23 21:42:48 +0700

    fasdf

---
+++ middleware/utils.js
@@ -0,0 +1,54 @@
+function calculateTotalQuantity(req, res, next) {
+    let totalQuantity = 0;
+    if (req.session.cart) {
+        for (let productId in req.session.cart) {
+            totalQuantity += req.session.cart[productId];
+        }
+    }
+    res.locals.carts = totalQuantity;
+    next();
+}
+
+// Tạo mã đơn hàng từ thời gian hiện tại và tổng tiền
+function generateOrderCode(tongTien) {
+    const now = new Date();
+    const day = now.getDate().toString().padStart(2, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const year = now.getFullYear().toString();
+    const hour = now.getHours().toString().padStart(2, '0');
+    const minute = now.getMinutes().toString().padStart(2, '0');
+    const second = now.getSeconds().toString().padStart(2, '0');
+
+    // Tạo mã đơn hàng
+    const orderCode = `${day}${month}${year}${hour}${minute}${second}`;
+    return orderCode;
+}
+
+function convertCartToString(cart) {
+    let cartString = '';
+    for (const [product, quantity] of Object.entries(cart)) {
+        cartString += `${product} - ${quantity}; `;
+    }
+    // Xóa dấu cách ở cuối chuỗi
+    cartString = cartString.trim();
+    return cartString;
+}
+
+function getCurrentDate() {
+    const now = new Date();
+    const day = now.getDate().toString().padStart(2, '0'); // Lấy ngày và đảm bảo định dạng là 2 chữ số, thêm '0' nếu cần
+    const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Lấy tháng và đảm bảo định dạng là 2 chữ số, thêm '0' nếu cần
+    const year = now.getFullYear(); // Lấy năm
+
+    // Kết hợp ngày, tháng và năm thành chuỗi định dạng "DD-MM-YYYY"
+    const currentDate = `${day}-${month}-${year}`;
+
+    return currentDate;
+}
+
+module.exports = {
+    calculateTotalQuantity,
+    generateOrderCode,
+    convertCartToString,
+    getCurrentDate
+};
--- route/home.js
+++ route/home.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const { dataProduct } = require('../src/config');
+const { calculateTotalQuantity, generateOrderCode, convertCartToString, getCurrentDate} = require('../middleware/utils')
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get("/", calculateTotalQuantity, async (req, res, next) => {
     try {
         const product = await dataProduct.find().sort({ _id: -1 }).limit(12);
-        res.render("index", { pros: product, userN: req.session.username, login: "login", logout: "logout" });  // Đặt tên view đúng
+        res.render("index", { pros: product,
+                             userN: req.session.username,
+                              login: "login",
+                               logout: "logout",
+                               carts: res.locals.carts });  // Đặt tên view đúng
     } catch (error) {
         next(error);  // Chuyển lỗi tới middleware xử lý lỗi
     }
 });
 
-/* router.get("/", (req, res) => {
-    res.redirect("/home");
-}); */
 
 module.exports = router;
